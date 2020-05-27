68ed31de3897d7625c6cebacf3dfbec61ace0c8c
CarlosJr <carlooosmercedes@gmail.com>
2020-05-26 21:05:28 -0600

    retorno

--- proyecto-2020-SelectSlav-master/controllers/deptController.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-    require "../models/DepartamentoModel.php";
-    $objDepartamento = new DepartamentoModel();
-    $datos = $objDepartamento->getAllDept();
-    require_once "../views/departamentos.php";
-?>
